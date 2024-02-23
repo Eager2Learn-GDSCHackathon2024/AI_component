287e509a9366734602e390dcc734e4c9615f3393
Trần Bá Hoàng <87892619+hoangbros03@users.noreply.github.com>
2024-02-23 18:56:52 +0000

    feat: Add install-able package, reqs, readme, main

---
+++ README.md
@@ -0,0 +1,10 @@
+# Instruction
+
+Intall necessary packages:
+```
+# Optional: Create new fresh isolated env with conda first.
+git clone https://github.com/Eager2Learn-GDSCHackathon2024/AI_component.git
+cd AI_component
+pip install -r requirements.txt
+pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="AI_component",
+    version="1.0",
+    description="Provide necessary AI component for Backend",
+    author="Tran Ba Hoang",
+    author_email="hoangtb203@gmail.com",
+    url="https://github.com/Eager2Learn-GDSCHackathon2024/AI_component",
+    classifiers=[
+        # List of classifiers describing the package (e.g., for PyPI)
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
